5ba5d16c0d3deb70fa3a85b39d6f6805e0c5e7d7
Viktoriia Malysh <pylypenko.viktoriia@gmail.com>
2022-08-21 17:31:01 +0200

    add 7-9 tasks code

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener("submit", processFormEl);
+
+    function processFormEl(event) {
+        event.preventDefault();
+            const {
+                elements: {email, password} } = event.currentTarget;
+                
+                if(email.value === "" || password.value === "") {
+                   
+                    return alert("Please fill out all the fields of the form");
+                } else {
+                    const formData = {};
+                        formData.email = email.value;
+                        formData.password = password.value;
+
+                        console.log(formData);
+                    event.currentTarget.reset();
+                }
+    };
